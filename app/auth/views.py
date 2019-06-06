e3b619913ce7896fab12c9988026ab8bf2878a4a
gichimux <gichimumwai1@gmail.com>
2019-06-06 12:07:00 +0300

    create login and signup views and templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from .forms import RegistrationForm, LoginForm
 from .. import db
 from . import auth
 from flask_login import login_user, logout_user, login_required
-from ..email import mail_message
+# from ..email import mail_message
 
 
 @auth.route('/login', methods=["GET","POST"])
@@ -27,11 +27,11 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, fname = form.fname.data,lname= form.lname.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to Pitcher", "email/welcome_user", user.email, user=user)
+        # mail_message("Welcome to Pitcher", "email/welcome_user", user.email, user=user)
         
         return redirect(url_for('auth.login'))
         title = "New Account"
